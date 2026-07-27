037799d316ddf22ab990535d8abe41a4e1f5a65b
slow70429-tech <slow70429@gmail.com>
2026-07-27 23:08:23 +0800

    V3.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='efa-v27-back-version';
+const CACHE='efa-v31-custom-points';
 const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
